9bad8d7660e2e7af44504b5803f04a1f59ca8147
Haile Mahalete <s2100670@edu.bc.fi>
2024-05-07 12:55:00 +0300

    Initial cmmit with backend working API setup

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,8 +1,5 @@
-// firebaseAdmin.js
 const admin = require('firebase-admin');
-
-const serviceAccount = require('.C:\Users\s2100670\Documents\newsHub\news-hub-backend\config\newshub-01-firebase-adminsdk-j7t1q-8dd3d22759.json');
-
+const serviceAccount = require('./config/newshub-01-firebase-adminsdk-j7t1q-8dd3d22759.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
